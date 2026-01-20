56e1d8da356d0fed89603c9276baf43f2c09929c
theeramat2060 <theeramat_m@cmu.ac.th>
2026-01-20 15:35:21 +0700

    task 9 For Loop
    -For Loop

---
+++ src/for.ts
@@ -0,0 +1,19 @@
+// let i = 0;
+// for (i = 0; i < 10; i++) {
+//     console.log(i);
+// }
+//
+//
+// let color = ['red', 'blue', 'green', 'yellow', 'orange', 'purple']
+// for (let i = 0; i < color.length; i++) {
+//     console.log(color[i])
+// }
+
+let name = ['van', 'blue', 'bomb', 'brook', 'orange']
+for (let i = 0; i < name.length; i++) {
+    if (name[i].length > 5){
+        console.log("สวัสดีคุณ ",name[i])
+    }else {
+        console.log("Hello ",name[i])
+    }
+}
