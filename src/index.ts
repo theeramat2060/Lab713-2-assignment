1f873f988247a7762de3ede23d9bb744158ecf38
theeramat2060 <theeramat_m@cmu.ac.th>
2026-01-20 15:23:54 +0700

    task 8 Calculate Grade
    Calculate Grade

--- src/index.ts
+++ src/index.ts
@@ -4,10 +4,34 @@
 // }
 
 
-let x:number = 4
-let y:number = 13
-if (x > 5 && y > 12 ) {
-    console.log('ค่ามากจัง')
-}else {
-    console.log((x*y)+(x/y))
+// let x:number = 4
+// let y:number = 13
+// if (x > 5 && y > 12 ) {
+//     console.log('ค่ามากจัง')
+// }else {
+//     console.log((x*y)+(x/y))
+// }
+
+
+// let x =10
+// if (x > 10) {
+//     console.log('x is more than 10')
+// } else if (x < 5){
+//     console.log('x is less than 5')
+// }
+// else {
+//     console.log('x is between 5 and 10')
+// }
+
+
+
+let x =80
+if (x < 50) {
+    console.log('Grade F')
+} else if (x < 60){
+    console.log('Grade C')
+} else if (x < 80){
+    console.log('Grade B')
+} else {
+    console.log('Grade A')
 }
