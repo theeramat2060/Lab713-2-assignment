4257273494bbd3906b844e7f6873d09e575a0452
theeramat2060 <theeramat_m@cmu.ac.th>
2026-01-20 14:59:45 +0700

    task 6 -data type
    data type

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,26 @@
-console.log("Hello, TypeScript with Node.js!");
+// console.log("Hello, TypeScript with Node.js!");
+
+// let x:number = 10;
+// x = 'hello';
+
+// let x:number | string = 10;
+// x = 'hello';
+//
+// let i = 0
+// i = 'hello'
+// console.log(i)
+
+let i = 0
+i = 'hello' as any
+console.log(i)
+
+
+let x:number | string = 10;
+x = 'hello';
+if (typeof x === 'string') {
+    console.log('x is a string');
+}else if (typeof x === 'number') {
+    console.log('x is a number');
+}else {
+    console.log('x is neither a string nor a number');
+}
