e1d6e02a1b57953f49c6b14bb9394dc727034e99
theeramat2060 <theeramat_m@cmu.ac.th>
2026-01-20 15:46:53 +0700

    task 10 For Loop 2
    for...in for...of

--- src/for.ts
+++ src/for.ts
@@ -9,11 +9,21 @@
 //     console.log(color[i])
 // }
 
-let name = ['van', 'blue', 'bomb', 'brook', 'orange']
-for (let i = 0; i < name.length; i++) {
-    if (name[i].length > 5){
-        console.log("สวัสดีคุณ ",name[i])
-    }else {
-        console.log("Hello ",name[i])
-    }
+// let name = ['van', 'blue', 'bomb', 'brook', 'orange']
+// for (let i = 0; i < name.length; i++) {
+//     if (name[i].length > 5){
+//         console.log("สวัสดีคุณ ",name[i])
+//     }else {
+//         console.log("Hello ",name[i])
+//     }
+// }
+
+// let color = ['red', 'blue', 'green', 'yellow', 'orange', 'purple']
+// for (const c of color) {
+//     console.log(c);
+// }
+
+let color = ['red', 'blue', 'green', 'yellow', 'orange', 'purple']
+for (const index in color) {
+    console.log(`Index: ${index}, Value: ${color[index]}`);
 }
