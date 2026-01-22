bcda8962b2bfadd512d10e022f5571b6ff17095a
theeramat2060 <theeramat_m@cmu.ac.th>
2026-01-22 20:49:16 +0700

    task 4 express
    express

--- src/server.ts
+++ src/server.ts
@@ -1,75 +1,37 @@
-import express, {Request, Response} from 'express'
-import dotenv from 'dotenv';
-dotenv.config();
-import eventRoute from "./routes/EventRoute";
-import path from 'path';
-import cors, {CorsOptions} from 'cors';
-import multer from 'multer';
-import {getPresignedUrl, uploadFile} from './services/UploadFileService';
-
+import express, { Request, Response } from 'express'
 const app = express()
 const port = 3000
-const webApp = express()
-const webPort = 5050
-const corsOptions: CorsOptions = {
-    origin: ['http://localhost:5050'],
-    methods: ['GET', 'POST', 'OPTIONS'],
-    allowedHeaders: ['Content-Type', 'Authorization'],
-};
 
-app.use(express.json())
-app.use(cors(corsOptions))
-app.use('/events', eventRoute);
-webApp.use(express.static(path.join(process.cwd())));
-webApp.listen(webPort, () => {
-    console.log(`WebApp listening at http://localhost:${webPort}`)
+app.get('/', (req: Request, res: Response) => {
+    res.send('Hello World!')
+})
+// app.get('/test', (req, res) => {
+//     res.send('Hello World! 3')
+// })
+
+app.get('/test', (req: Request, res: Response) => {
+    const id = req.query.id;
+    const output = `id: ${id}`;
+    res.send(output);
 })
 
+app.get('/test', (req, res) => {
+       let returnObj = {
+               name: 'test',
+               age: 20,
+               address: 'Thai'
+       }
+       res.send(returnObj);
+})
 
+app.get('/test', (req: Request, res: Response) => {
+    const id = req.query.id;
+    const output = `id: ${id}`;
+    res.send(output);
+})
 
-const upload = multer({storage: multer.memoryStorage()});
-app.post('/upload', upload.single('file'), async (req: any, res: any) => {
-    try {
-        const file = req.file;
-        if (!file) {
-            return res.status(400).send('No file uploaded.');
-        }
-        const bucket = process.env.SUPABASE_BUCKET_NAME;
-        const filePath = process.env.UPLOAD_DIR;
-        if (!bucket || !filePath) {
-            return res.status(500).send('Bucket name or file path not configured.');
-        }
-        const outputUrl = await uploadFile(bucket, filePath, file);
-        res.status(200).send(outputUrl);
-    } catch (error) {
-        res.status(500).send('Error uploading file.');
-    }
-});
-
-
-app.get('/presignedUrl', async (req: Request, res: Response) => {
-    console.log("Generating presigned URL for file key:");
-    try {
-        const {key} = req.query;
-        if (!key || typeof key !== 'string') {
-            return res.status(400).send('File key is required.');
-        }
-        const bucket = process.env.SUPABASE_BUCKET_NAME;
-        if (!bucket) {
-            return res.status(500).send('Bucket or file path not configured.');
-        }
-        const presignedUrl = await getPresignedUrl(bucket, key, 3600);
-        ;
-        res.status(200).json({url: presignedUrl});
-    } catch (error) {
-        console.error('Error generating presigned URL:', error);
-        res.status(500).send('Error generating presigned URL.');
-    }
-});
-app.get('/vanilla', (req, res) => {
-    res.send("vanilla js response");
-});
 
 app.listen(port, () => {
     console.log(`App listening at http://localhost:${port}`)
 })
+
