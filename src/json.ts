546cff348d3dfeab158debd4796f982daf9c4c22
theeramat2060 <theeramat_m@cmu.ac.th>
2026-01-22 20:21:44 +0700

    task 15 JavaScript Object Notation (JSON)
    JavaScript Object Notation (JSON)

---
+++ src/json.ts
@@ -0,0 +1,46 @@
+// const object = {
+//     name: "John",
+//     age: 30,
+//     city: "New York"
+// }
+//
+// console.log(object)
+
+
+// const object = {
+//     name: "John",
+//     age: 30,
+//     city: "New York"
+// }
+//
+// console.log(object.name)
+
+//
+// const object = {
+//     name: "John",
+//     age: 30,
+//     city: "New York"
+// }
+//
+// console.log(object.name)
+// const jsonStr = JSON.stringify(object);
+// console.log("Object: ", object)
+// console.log("json String: ", jsonStr)
+
+
+const objectStudent = {
+    firstName: "Somchai",
+    lastName: "Sukjai",
+    age: 20,
+    gpa: 1.5
+}
+
+function checkStudentStatus(student: {firstName: string, lastName: string, age: number, gpa: number}): string {
+    if (student.gpa > 2) {
+        return JSON.stringify(student);
+    } else {
+        return `คุณ ${student.firstName} พ้นสภาพ`;
+    }
+}
+
+console.log(checkStudentStatus(objectStudent));
