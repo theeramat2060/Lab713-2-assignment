f8f8e00817d18909932ff63a6f541aa4c5dcfe87
theeramat2060 <theeramat_m@cmu.ac.th>
2026-01-22 20:49:16 +0700

    task 4 express
    express

--- src/server.ts
+++ src/server.ts
@@ -2,50 +2,36 @@ import express, { Request, Response } from 'express'
 const app = express()
 const port = 3000
 
-app.use(express.json())
-
 app.get('/', (req: Request, res: Response) => {
-    res.json('Hello World!')
+    res.send('Hello World!')
+})
+// app.get('/test', (req, res) => {
+//     res.send('Hello World! 3')
+// })
+
+app.get('/test', (req: Request, res: Response) => {
+    const id = req.query.id;
+    const output = `id: ${id}`;
+    res.send(output);
 })
 
-app.get('/book', (req: Request, res: Response) => {
-    res.json(books);
+app.get('/test', (req, res) => {
+       let returnObj = {
+               name: 'test',
+               age: 20,
+               address: 'Thai'
+       }
+       res.send(returnObj);
 })
 
+app.get('/test', (req: Request, res: Response) => {
+    const id = req.query.id;
+    const output = `id: ${id}`;
+    res.send(output);
+})
 
 
 app.listen(port, () => {
     console.log(`App listening at http://localhost:${port}`)
 })
 
-interface Book {
-    id: number;
-    title: string;
-    Author_name: string;
-    description: string;
-    groups: string[]; // e.g. genres or categories
-}
-
-const books: Book[] = [
-    {
-        id: 1,
-        title: "Learning TypeScript",
-        Author_name: "John Doe",
-        description: "A practical guide to TypeScript for modern web development.",
-        groups: ["Programming", "Web", "TypeScript"]
-    },
-    {
-        id: 2,
-        title: "Node.js in Action",
-        Author_name: "Jane Smith",
-        description: "Hands-on examples and best practices for building Node.js apps.",
-        groups: ["Programming", "Backend", "Node.js"]
-    },
-    {
-        id: 3,
-        title: "Clean Code",
-        Author_name: "Robert C. Martin",
-        description: "A handbook of agile software craftsmanship.",
-        groups: ["Programming", "Software Engineering", "Best Practices"]
-    }
-]
