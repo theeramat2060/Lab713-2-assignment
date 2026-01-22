025135934ea11d7abb82923f5226fbbcdcd7dda1
theeramat2060 <theeramat_m@cmu.ac.th>
2026-01-22 23:08:32 +0700

    task 3 async-await
    async-await

--- src/server.ts
+++ src/server.ts
@@ -12,46 +12,37 @@ app.get('/', (req: Request, res: Response) => {
 })
 
 
-app.get("/events/:id", (req, res) => {
+app.get("/events/:id", async (req, res) => {
     const id = parseInt(req.params.id);
-    getEventById(id)
-        .then((event: Event | undefined) => {
-            if (event) {
-                res.json(event);
-            } else {
-                res.status(404).send("Event not found");
-            }
-        });
-
-
-});
-
-
-app.get("/events", (req, res) => {
-    if (req.query.category) {
-        const category = req.query.category as string;
-        getEventByCategory(category)
-            .then((events: Event[]) => {
-                res.json(events);
-            });
+    const event = await getEventById(id);
+    if (event) {
+        res.json(event);
     } else {
-        getAllEvents().then((events) => {
-            res.send(events)
-        });
+        res.status(404).send("Event not found");
     }
 });
 
 
-app.post("/events", (req, res) => {
-    const newEvent: Event = req.body;
-    addEvent(newEvent)
-        .then((event: Event) => {
-            res.json(event);
-        });
+app.get("/events", async (req, res) => {
+    if (req.query.category) {
+        const category = req.query.category as string;
+        const filteredEvents = await getEventByCategory(category);
+        res.json(filteredEvents);
+    } else {
+        res.json(await getAllEvents());
+    }
 });
 
 
-    app.listen(port, () => {
-        console.log(`App listening at http://localhost:${port}`)
-    })
+app.post("/events", async (req, res) => {
+    const newEvent: Event = req.body;
+
+
+    res.json(await addEvent(newEvent));
+});
+
+
+app.listen(port, () => {
+    console.log(`App listening at http://localhost:${port}`)
+})
 
