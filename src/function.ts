955f9861d070b0db3534c79645a14da5c78b58c6
theeramat2060 <theeramat_m@cmu.ac.th>
2026-01-22 20:08:58 +0700

    task 13 functions 3
    functions 3

--- src/function.ts
+++ src/function.ts
@@ -41,11 +41,45 @@
 // console.log(multiply(5, 10))
 
 
-const multiply = (x,y) => {
-        if (x>y){
-            return x;
-        }else {
-            return y;
+// const multiply = (x,y) => {
+//         if (x>y){
+//             return x;
+//         }else {
+//             return y;
+//         }
+// }
+// console.log(multiply(13, 10))
+
+// const add = (a,b) => {
+//     return a+b;
+// }
+// console.log(add(1,2));
+
+// const add = (a:number,b:number) => {
+//     return a+b;
+// }
+// const result = add(1,2) + 0;
+// console.log(result, 'type of result:', typeof result);
+
+// const add = (a:number,b:number):string => {
+//     return a+b;
+// }
+// const result = add(1,2) + 0;
+// console.log(result, 'type of result:', typeof result);
+
+// const add = (a:number,b:number):string => {
+//     const result =  a+b;
+//     return result.toString();
+// }
+
+let numList=[10,20,30,40,50,77,5,3,99,23];
+const findMax = (numList):number => {
+    let max = numList[0];
+    for (let i = 1; i < numList.length; i++) {
+        if (numList[i] > max) {
+            max = numList[i];
         }
+    }
+    return max;
 }
-console.log(multiply(13, 10))
+console.log('Max number is:', findMax(numList));
