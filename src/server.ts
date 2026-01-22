982b62d03a072107758701d1bac84cbf06c97a17
theeramat2060 <theeramat_m@cmu.ac.th>
2026-01-22 22:42:13 +0700

    task 1 refactor code to function
    refactor code to function

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,5 @@
-import express, { Request, Response } from 'express'
+import express, {Request, Response} from 'express'
+
 const app = express()
 app.use(express.json())
 const port = 3000
@@ -73,31 +74,45 @@ const events: Event[] = [
     }
 ]
 
+function getEventByCategory(category: string): Event[] {
+    const filteredEvents = events.filter((event) => event.category === category);
+    return filteredEvents;
+}
+
+function getAllEvents(): Event[] {
+    return events;
+}
+
+function getEventById(id: number): Event | undefined {
+    return events.find((event) => event.id === id);
+}
+
+function addEvent(newEvent: Event): Event {
+    newEvent.id = events.length + 1;
+    events.push(newEvent);
+    return newEvent;
+}
+
 
 app.get('/', (req: Request, res: Response) => {
     res.send('Hello World!')
 })
 
-// app.get("/events", (req, res) => {
-//     console.log("Query Params:", req.query);
-//     const category = req.query.category;
-//     const filteredEvents = events.filter((event) => event.category === category);
-//     res.json(filteredEvents);
-// });
 
 app.get("/events", (req, res) => {
     if (req.query.category) {
-        const category = req.query.category;
-        const filteredEvents = events.filter((event) => event.category === category);
+        const category = req.query.category as string;
+        const filteredEvents = getEventByCategory(category);
         res.json(filteredEvents);
     } else {
-        res.json(events);
+        res.json(getAllEvents());
     }
 });
 
+
 app.get("/events/:id", (req, res) => {
     const id = parseInt(req.params.id);
-    const event = events.find((event) => event.id === id);
+    const event = getEventById(id);
     if (event) {
         res.json(event);
     } else {
@@ -107,29 +122,9 @@ app.get("/events/:id", (req, res) => {
 
 app.post("/events", (req, res) => {
     const newEvent: Event = req.body;
-    newEvent.id = events.length + 1;
-    events.push(newEvent);
+    addEvent(newEvent);
     res.json(newEvent);
 });
-
-
-
-
-app.get('/test', (req, res) => {
-       let returnObj = {
-               name: 'test',
-               age: 20,
-               address: 'Thai'
-       }
-       res.send(returnObj);
-})
-
-app.get('/test', (req: Request, res: Response) => {
-    const id = req.query.id;
-    const output = `id: ${id}`;
-    res.send(output);
-})
-
 
 app.listen(port, () => {
     console.log(`App listening at http://localhost:${port}`)
