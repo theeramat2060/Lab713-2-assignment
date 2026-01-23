82b5834958069ee5c9c661e9e191bd35e2469179
theeramat2060 <theeramat_m@cmu.ac.th>
2026-01-23 19:30:08 +0700

    task 1 Prisma setup
    Prisma setup

--- prisma.config.ts
+++ prisma.config.ts
@@ -5,11 +5,9 @@ import { defineConfig } from "prisma/config";
 
 export default defineConfig({
   schema: "prisma/schema.prisma",
-    migrations: {
-        path: "prisma/migrations",
-        seed: 'tsx prisma/seed.ts'
-    },
-
+  migrations: {
+    path: "prisma/migrations",
+  },
   datasource: {
     url: process.env["DATABASE_URL"],
   },
